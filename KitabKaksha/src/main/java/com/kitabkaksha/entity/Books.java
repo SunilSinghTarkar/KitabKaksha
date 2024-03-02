5d97f9d262ae415946790c7321fab811a502fba1
sunilsingh tarkar <sunilthakur89544@gmail.com>
2024-03-02 20:16:13 +0530

    validation

--- KitabKaksha/src/main/java/com/kitabkaksha/entity/Books.java
+++ KitabKaksha/src/main/java/com/kitabkaksha/entity/Books.java
@@ -13,6 +13,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -21,12 +24,29 @@ public class Books {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int bookId;
+
+	@NotBlank(message = "Book Name cannot be blank")
+	@Size(max = 25, message = "Book Name must be at most 25 characters")
 	private String title;
+
+	@NotBlank(message = "Author Name cannot be blank")
+	@Size(max = 25, message = "Author Name must be at most 25 characters")
 	private String author;
+
+	@NotBlank(message = "Genre cannot be blank")
+	@Size(max = 20, message = "Genre must be at most 20 characters")
 	private String genre;
+
+	@NotBlank(message = "Description cannot be blank")
+	@Size(max = 50, message = "Description must be at most 50 characters")
 	private String description;
+
+	@Positive(message = "Price must be a positive value")
 	private double price;
+
 	private String imageUrl;
+
+	@Positive(message = "Quantity must be a positive value")
 	private int quantity;
 	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private String publisher;
